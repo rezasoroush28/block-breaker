5f9bca905e2caf2b70621e74e1d335d83d296abf
reza <rezsoroush28@gmail.com>
2022-10-27 12:56:22 +0330

    redemtion and start

---
+++ Assets/LevelModel.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "LevelModel" , order = 1)]
+public class LevelModel : ScriptableObject
+{
+    
+}
