4e563afe2cdf47ffa0e3730425fc63ae11de27ac
reza <rezsoroush28@gmail.com>
2022-10-27 17:43:21 +0330

    levelmodel

--- Assets/LevelModel.cs
+++ Assets/LevelModel.cs
@@ -1,8 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+
 [CreateAssetMenu(fileName = "LevelModel" , order = 1)]
 public class LevelModel : ScriptableObject
 {
+    //This is Level 1
+    public abstract class Brick
+    {
+        //int health
+        //bool isAvailable
+    }
     
+    
+    [System.Serializable] public class BrickRows
+    { 
+        [SerializeField] private List<Brick> _listRows;
+    }
+
+    [SerializeField] private List<BrickRows> allBricks;
+
+
 }
