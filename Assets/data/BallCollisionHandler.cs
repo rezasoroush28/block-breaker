47bc78544cf3cd57f774935a146a9f980c3dadbc
reza <rezsoroush28@gmail.com>
2022-12-04 12:49:26 +0330

    collision physics handed

--- Assets/data/BallCollisionHandler.cs
+++ Assets/data/BallCollisionHandler.cs
@@ -7,9 +7,24 @@ namespace data
     public class BallCollisionHandler : MonoBehaviour
     {
         public Viewer viewer;
-        
+        private Rigidbody2D _rb;
+
+        private void Start()
+        {
+            _rb = transform.GetComponent<Rigidbody2D>();
+        }
+
         private void OnTriggerEnter2D(Collider2D other)
         {
+            
+
+            var collidPose = other.ClosestPoint(transform.position);
+            var normal = collidPose - (Vector2)transform.position;
+            normal = new Vector2(normal.x, normal.y);
+            var velocity = transform.GetComponent<Rigidbody2D>().velocity; 
+            var mirror = Mirror(velocity, normal);
+            transform.GetComponent<Rigidbody2D>().velocity = new Vector2(mirror.x, mirror.y);
+            
             if (other.CompareTag($"block"))
             {
                 var attacher = other.GetComponent<BlockData>();
@@ -17,21 +32,19 @@ namespace data
                 attacher.presenterForThisBlock.Notify();
             }
 
-            var collidPose = other.ClosestPoint(transform.position);
-            var normal = collidPose - (Vector2)transform.position;
-            normal = new Vector2(normal.x, normal.y);
-            var velocity = transform.GetComponent<Rigidbody2D>().velocity; 
-            transform.GetComponent<Rigidbody2D>().velocity = Mirror(velocity, normal);
-
         }
 
         private Vector2 Mirror(Vector2 v1, Vector2 normal)
         {
-            var n = normal / normal.magnitude;
-            v1 -= Vector2.Dot(n, v1) * n;
+            var n = normal.normalized;
+            v1 -= Vector2.Dot(n, v1) * n *2f;
             return v1;
         }
 
-        
+        private void Update()
+        {
+            var v = _rb.velocity;
+            Debug.Log(v);
+        }
     }
 }
